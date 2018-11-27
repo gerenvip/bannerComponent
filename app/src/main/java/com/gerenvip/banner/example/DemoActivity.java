fb6491d8c60de0ed6b37ea019d65cd94fba9c611
wangwei <wangwei@jiandaola.com>
2018-11-27 11:08:09 +0800

    fix

--- app/src/main/java/com/gerenvip/banner/example/DemoActivity.java
+++ app/src/main/java/com/gerenvip/banner/example/DemoActivity.java
@@ -3,8 +3,13 @@ package com.gerenvip.banner.example;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.ToxicBakery.viewpager.transforms.CubeInTransformer;
+import com.ToxicBakery.viewpager.transforms.DefaultTransformer;
 import com.gerenvip.banner.R;
 
 import java.util.ArrayList;
@@ -19,12 +24,42 @@ public class DemoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demo);
         mBannerView = (DemoBannerView) findViewById(R.id.banner_view);
-        mBannerView.enableScroll(true);
-        mBannerView.setBannerInterval(3000l);
-        mBannerView.getViewPager().setPageTransformer(false, new CubeInTransformer());
+        mBannerView.enableAutoScroll(true);
+        mBannerView.setBannerPeriod(3000l);
+        mBannerView.setBannerScheduleDelay(2000);
+        mBannerView.getViewPager().setPageTransformer(false, new DefaultTransformer());
 
         List<DemoBannerView.Item> list = createList();
         mBannerView.setData(list);
+
+        FrameLayout bannerContent = findViewById(R.id.banner_content);
+
+        DemoBannerView bannerView = new DemoBannerView(this);
+        bannerContent.addView(bannerView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        bannerView.enableAutoScroll(true);
+        bannerView.setBannerPeriod(4000);
+        bannerView.setPageTransformer(new CubeInTransformer());
+        List<DemoBannerView.Item> data = createList();
+        bannerView.setData(data);
+    }
+
+    public void startTimer(View view) {
+        boolean state = mBannerView.startAutoScroll();
+        Toast.makeText(this, "restart -> " + state, Toast.LENGTH_SHORT).show();
+    }
+
+    public void stopTimer(View view) {
+        mBannerView.stopAutoScroll();
+        Toast.makeText(this, "stop timer ", Toast.LENGTH_SHORT).show();
+    }
+
+    public void toggle(View view) {
+        boolean autoScrollEnable = mBannerView.isAutoScrollEnable();
+        mBannerView.enableAutoScroll(!autoScrollEnable);
+        if (!autoScrollEnable) {
+            mBannerView.startAutoScroll();
+        }
+        Toast.makeText(this, autoScrollEnable ? "stop scroll ok " : "start scroll ok", Toast.LENGTH_SHORT).show();
     }
 
     public List<DemoBannerView.Item> createList() {
--- banner/src/main/java/com/gerenvip/banner/BannerView.java
+++ banner/src/main/java/com/gerenvip/banner/BannerView.java
@@ -1,5 +1,6 @@
 package com.gerenvip.banner;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Handler;
@@ -7,6 +8,7 @@ import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -29,23 +31,17 @@ public abstract class BannerView<T> extends FrameLayout implements Runnable {
 
     private BaseBannerAdapter<T> mBannerAdapter;
 
-    private static final long INTERVAL_DEFAULT = 2000l;
+    private static final long INTERVAL_DEFAULT = 2000;
 
-    private long mBannerInterval = INTERVAL_DEFAULT;
+    private long mScheduleDelay = INTERVAL_DEFAULT;
     private long mBannerPeriod = INTERVAL_DEFAULT;
 
-    private TimerTask mTimerTask = new TimerTask() {
-        @Override
-        public void run() {
-            if (!mIsUserTouched) {
-                mHandler.post(BannerView.this);
-            }
-        }
-    };
+    private TimerTask mTimerTask;
     private boolean mHideIndicator;
     private int mIndicatorHeight;
     private boolean mTimerCancel;
-    private boolean mEnableScroll = true;
+    private boolean mEnableAutoScroll = true;
+    private boolean mRunning;
 
     public BannerView(Context context) {
         super(context);
@@ -61,73 +57,9 @@ public abstract class BannerView<T> extends FrameLayout implements Runnable {
         init();
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.base_banner_view, this, true);
-        mTimer = new Timer();
-    }
-
-    /**
-     * before {@link #setData(List)}
-     *
-     * @param time
-     */
-    public void setBannerInterval(long time) {
-        mBannerInterval = time;
-    }
-
-    /**
-     * before {@link #setData(List)}
-     *
-     * @param time
-     */
-    public void setBannerPeriod(long time) {
-        mBannerPeriod = time;
-    }
-
-    public void enableScroll(boolean enableScroll) {
-        mEnableScroll = enableScroll;
-    }
-
-    public void setData(List<T> list) {
-        mBannerAdapter.setData(list);
-        mIndicator.setCount(list.size());
-        scheduleScroll();
-    }
-
-    private void scheduleScroll() {
-        if (!mEnableScroll) {
-            return;
-        }
-        mTimer.schedule(mTimerTask, mBannerInterval, mBannerPeriod);
-    }
-
-    public void notifyDataSetChanged() {
-        mBannerAdapter.notifyDataSetChanged();
-        int dataSize = mBannerAdapter.getDataSize();
-        mIndicator.setCount(dataSize);
-    }
-
-    public void setCurrent(int current) {
-        mViewPager.setCurrentItem(current);
-    }
-
-    public int getCurrent() {
-        return mViewPager.getCurrentItem();
-    }
-
-    public void hideIndicator(boolean hide) {
-        mHideIndicator = hide;
-        mIndicator.setVisibility(mHideIndicator ? GONE : VISIBLE);
-    }
-
-    public void setIndicatorHeight(int height) {
-        mIndicator.getLayoutParams().height = height;
-        mIndicator.requestLayout();
-    }
-
-    @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
         mViewPager = (ViewPager) findViewById(R.id.banner);
         mIndicator = (PagerIndicator) findViewById(R.id.indicator);
         if (mHideIndicator) {
@@ -146,14 +78,190 @@ public abstract class BannerView<T> extends FrameLayout implements Runnable {
                 int action = event.getAction();
                 if (action == MotionEvent.ACTION_DOWN
                         || action == MotionEvent.ACTION_MOVE) {
+                    mHandler.removeCallbacks(mDelayResetRunnable);
                     mIsUserTouched = true;
-                } else if (action == MotionEvent.ACTION_UP) {
-                    mIsUserTouched = false;
+                } else if (action == MotionEvent.ACTION_UP
+                        || action == MotionEvent.ACTION_CANCEL) {
+//                    mIsUserTouched = false;
+                    mHandler.removeCallbacks(mDelayResetRunnable);
+                    mHandler.postDelayed(mDelayResetRunnable, 1000);
                 }
                 return false;
             }
         });
     }
+
+    /**
+     * before {@link #setData(List)}
+     *
+     * @param time
+     */
+    public void setBannerScheduleDelay(long time) {
+        mScheduleDelay = time;
+    }
+
+    /**
+     * before {@link #setData(List)}
+     *
+     * @param time
+     */
+    public void setBannerPeriod(long time) {
+        mBannerPeriod = time;
+    }
+
+    public void enableAutoScroll(boolean enableScroll) {
+        boolean oldValue = mEnableAutoScroll;
+        mEnableAutoScroll = enableScroll;
+        adjustAutoScroll(oldValue);
+    }
+
+    public boolean isAutoScrollEnable() {
+        return mEnableAutoScroll;
+    }
+
+    private void adjustAutoScroll(boolean oldValue) {
+        if (oldValue != mEnableAutoScroll) {
+            if (!mEnableAutoScroll) {
+                stopAutoScroll();
+            } else {
+                Log.w("BannerView", "please call startAutoScroll to real start auto scroll");
+            }
+        }
+    }
+
+    public void setData(List<T> list) {
+        mBannerAdapter.setData(list);
+        mIndicator.setCount(list.size());
+        startTimer();
+    }
+
+    private void scheduleScroll() {
+        if (!mEnableAutoScroll) {
+            return;
+        }
+        mTimer.schedule(mTimerTask, mScheduleDelay, mBannerPeriod);
+        mRunning = true;
+    }
+
+    private void ensureTimer() {
+        if (mTimer == null) {
+            mTimer = new Timer();
+        }
+        if (mTimerTask == null) {
+            mTimerTask = createTask();
+        }
+    }
+
+    private TimerTask createTask() {
+        return new TimerTask() {
+            @Override
+            public void run() {
+                if (!mIsUserTouched) {
+                    mHandler.post(BannerView.this);
+                } else {
+                    mHandler.removeCallbacks(BannerView.this);
+                }
+            }
+        };
+    }
+
+    /**
+     * 调用关闭自动滚动
+     */
+    public void stopAutoScroll() {
+        stopTimer();
+    }
+
+    private void stopTimer() {
+        try {
+            if (mTimer != null) {
+                mTimer.cancel();
+                mTimer = null;
+                mTimerCancel = true;
+                mTimerTask.cancel();
+                mTimerTask = null;
+            }
+            mRunning = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startTimer() {
+        if (!mEnableAutoScroll) {
+            return;
+        }
+        if (isScheduleRunning()) {
+            Log.i("BannerView", "Timer already running");
+            return;
+        }
+        ensureTimer();
+        scheduleScroll();
+    }
+
+    private boolean isScheduleRunning() {
+        return mRunning;
+    }
+
+    /**
+     * 调用开启自动滚动
+     */
+    public boolean startAutoScroll() {
+        if (!mEnableAutoScroll) {
+            LogUtil.d("BannerView", "can not startTimer, you must call enableAutoScroll() to enable auto scroll");
+            return false;
+        }
+        startTimer();
+        return true;
+    }
+
+    public void notifyDataSetChanged() {
+        mBannerAdapter.notifyDataSetChanged();
+        int dataSize = mBannerAdapter.getDataSize();
+        mIndicator.setCount(dataSize);
+    }
+
+    public void setCurrent(int current) {
+        mViewPager.setCurrentItem(current);
+    }
+
+    public int getCurrent() {
+        return mViewPager.getCurrentItem();
+    }
+
+    /**
+     * 是否隐藏指示器
+     *
+     * @param hide true 隐藏，反之 显示
+     */
+    public void hideIndicator(boolean hide) {
+        mHideIndicator = hide;
+        mIndicator.setVisibility(mHideIndicator ? GONE : VISIBLE);
+    }
+
+    /**
+     * 设置指示器的高度
+     *
+     * @param height px
+     */
+    public void setIndicatorHeight(int height) {
+        mIndicator.getLayoutParams().height = height;
+        mIndicator.requestLayout();
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+    }
+
+    private Runnable mDelayResetRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mIsUserTouched = false;
+        }
+    };
 
     public ViewPager getViewPager() {
         return mViewPager;
@@ -169,6 +277,10 @@ public abstract class BannerView<T> extends FrameLayout implements Runnable {
 
     @Override
     public void run() {
+        int dataSize = mBannerAdapter.getDataSize();
+        if (dataSize <= 1) {
+            return;
+        }
         int nextIndex = mBannerAdapter.getNextIndex();
         if (nextIndex == mBannerAdapter.getCount() - 1) {
             nextIndex = mBannerAdapter.getDataSize() - 1;
@@ -182,15 +294,14 @@ public abstract class BannerView<T> extends FrameLayout implements Runnable {
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         if (mTimer != null && mTimerCancel) {
-            scheduleScroll();
+            startTimer();
         }
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
-        mTimer.cancel();
-        mTimerCancel = true;
+        stopTimer();
     }
 
     protected class BannerAdapter extends BaseBannerAdapter<T> {
